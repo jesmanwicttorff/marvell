1764a29bd61206e0b1ab2522a92a20451cca2661
jesmanwicttorff <jwarteaga@hotmail.com>
2023-11-22 21:12:32 -0300

    Initial version of MarvellBack generated by generator-jhipster@8.0.0

---
+++ src/test/java/com/marvell/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.marvell.security.jwt;
+
+import com.marvell.config.SecurityConfiguration;
+import com.marvell.config.SecurityJwtConfiguration;
+import com.marvell.config.WebConfigurer;
+import com.marvell.management.SecurityMetersService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface AuthenticationIntegrationTest {
+}
